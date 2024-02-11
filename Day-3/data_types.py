6f584e1f3e2441601b27b5360803ba648bc67cf3
Yana Rojiwadiya <132919342+YanaRojiwadiya@users.noreply.github.com>
2024-02-11 22:14:28 +0530

    Data Structure HandsOn

---
+++ Day-3/data_types.py
@@ -0,0 +1,43 @@
+#Create an integer variable and a float variable, and add them together and Check output type
+
+A = int(20)
+B = float(30.50)
+
+add = A + B
+print(add)
+print(type(add))
+
+#Create a string variable containing your name, and concatenate it with a string variable containing your favorite color
+
+name = "yana"
+color = "white"
+
+concate = name + color
+print(concate)
+
+#Convert a string variable containing the value "10" to an integer
+
+a = "10"
+a = int(a)
+print(a)
+print(type(a))
+
+#Create a list containing your favorite books
+
+books = ["bhagvat gita","ramayan","mahabharat"]
+print(books)
+print(type(books))
+
+#Create a tuple containing the numbers 1, 3, and 5
+
+Num = (1,3,5)
+print(Num)
+print(type(Num))
+
+#Create a dictionary containing the key-value pairs for your name, age, and hometown
+
+info = {"name":"yana","age":21,"hometown":"surat"}
+print(info)
+print(type(info))
+
+
